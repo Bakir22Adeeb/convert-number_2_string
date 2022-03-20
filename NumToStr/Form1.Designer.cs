cb3700979ce1998ac3b5c801b00eb6d08ea2b8b0
Bakir22Adeeb <abo.bakir22@gmail.com>
2022-03-20 15:12:40 +0300

    convert number to string

---
+++ NumToStr/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace NumToStr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.nudNumber = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMsg.Location = new System.Drawing.Point(12, 207);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(451, 37);
+            this.lblMsg.TabIndex = 2;
+            this.lblMsg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nudNumber
+            // 
+            this.nudNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudNumber.Location = new System.Drawing.Point(146, 86);
+            this.nudNumber.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+            this.nudNumber.Name = "nudNumber";
+            this.nudNumber.Size = new System.Drawing.Size(152, 36);
+            this.nudNumber.TabIndex = 3;
+            this.nudNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nudNumber.ValueChanged += new System.EventHandler(this.nudNumber_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 325);
+            this.Controls.Add(this.nudNumber);
+            this.Controls.Add(this.lblMsg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.NumericUpDown nudNumber;
+    }
+}
+
---
+++ NumToStr/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumToStr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> lsResult = new List<string>();
+        string[] single = { "صفر", "واحد", "اثنين", "ثلاثة", "اربعة", "خمسة", "ستة", "سبعة", "ثمانية", "تسعة" };
+        string[] et = {"", "احدا عشر", "اثنا عشر" };
+        string[] tens = { "", "عشرة", "عشرين", "ثلاثين", "اربعين", "خمسين", "ستين", "سبعين", "ثمانين", "تسعين" };
+        string[] hundred = { "", "مئة", "مئتان" };
+        public string num2str(int num, int len)
+        {
+            string temp = " ";
+            int x0, x1, x2;
+            switch (len)
+            {
+                case 1:
+                    {
+                        temp = single[num];
+                        break;
+                    }
+                case 2:
+                    {
+                        x0 = num % 10;
+                        num=num/10;
+                        x1 = num % 10;
+                        temp = tens_str(x1, x0);
+                        break;
+                    }
+                case 3:
+                    {
+                        x0 = num % 10;
+                        num = num / 10;
+                        x1 = num % 10;
+                        num = num / 10;
+                        x2 = num % 10;
+                        if(x2<=2)
+                        {
+                            if (x0 == 0 && x1 == 0)
+                            {
+                                temp = hundred[x2];
+                            }
+                            else
+                            {
+                                temp = hundred[x2] + " و " + tens_str(x1, x0);
+                            }
+                        }
+                         else if(x2>2)
+                        {
+                             if(x0==0 && x1==0)
+                             {
+                                 temp =single[x2]+ hundred[1];
+                             }
+                             else
+                             {
+                                 temp = single[x2] +" "+ hundred[1] + " و " + tens_str(x1, x0);
+                             }
+                         }
+                        break;
+                    }
+            }
+            return temp;
+        }
+        public string tens_str(int x1,int x0)
+        {
+            string temp = "";
+            if (x0 == 1 && x1 == 1)
+            {
+                temp = et[x0];
+            }
+            else if (x0 == 2 && x1 == 1)
+            {
+                temp = et[x0];
+            }
+            else if (x0 == 0 && x1 >0)
+            {
+                temp = tens[x1];
+            }
+            else if(x0>=1 && x1==0)
+            {
+                temp = single[x0];
+            }
+            else
+            {
+                if (x1 > 1)
+                    temp = single[x0] + " و " + tens[x1];
+                else
+                {
+                    temp = single[x0] + " " + tens[x1];
+                }
+            }     
+            return temp;
+        }
+        private void nudNumber_ValueChanged(object sender, EventArgs e)
+        {
+            int x, len = 0;
+            x = Int32.Parse(nudNumber.Value.ToString());
+            while (x != 0)
+            {
+                x /= 10;
+                len++;
+            }// end while
+            lblMsg.Text = num2str(Int32.Parse(nudNumber.Value.ToString()), len);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
